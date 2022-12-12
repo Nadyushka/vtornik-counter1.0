d8478f9a863b9fc9a9229051aaf7aef23db18d12
Nadyushka <12nadya11@gmail.com>
2022-12-12 21:18:43 +0300

    start top display

--- src/components/TopWindow/TopWindow.tsx
+++ src/components/TopWindow/TopWindow.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import s from './topWindow.module.css'
 
 type PropsType = {
     topWindowType: "minMaxSettings" | "presentation"
 }
 
-const TopWindow = (props:PropsType) => {
+const TopWindow = (props: PropsType) => {
+
+    const [minvalue, setMinValue] = useState<string>('0')
+    const [maxvalue, setMaxValue] = useState<string>('0')
+    const [value, setValue] = useState<string>('0')
+
+    const onChangeInputHandler = () => {
+    }
+
+    const minMaxWindowSet = () => {
+        return (
+            <div className={s.minMaxWindow}>
+                <form>
+                    <label htmlFor="minQuantity">Set min value:</label>
+                    <input type="number"
+                           id="minQuantity"
+                           className={s.minQuantity}
+                           onChange={e => setMinValue(e.currentTarget.value)}
+                           value={minvalue}
+                    />
+                </form>
+                <br/>
+                <form>
+                    <label htmlFor="maxQuantity">Set max value:</label>
+                    <input type="number"
+                           id="maxQuantity"
+                           className={s.maxQuantity}
+                           onChange={e => setMaxValue(e.currentTarget.value)}
+                           value={maxvalue}
+                    />
+                </form>
+            </div>)
+    }
+
+    const presentationWindowSet = () => {
+        return (
+            <div>{value}</div>
+        )
+    }
+
+
     return (
         <div className={s.topWindow}>
-            
+            {props.topWindowType === 'minMaxSettings' ? minMaxWindowSet() : presentationWindowSet()}
         </div>
     );
 };
